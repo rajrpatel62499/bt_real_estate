9f309369073ae256e42f47b4f8cf16fcdfffa517
Rajrpatel <rajrpatel62499@gmail.com>
2020-04-11 23:41:01 +0530

    changed settings and made django_env

--- btre/settings.py
+++ btre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "c^&*crfk26o0k(1h*w70ya(p+tl)r*9_ft+%vm!dye8(#zu1el"
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -141,3 +143,6 @@ EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+
+
+django_heroku.settings(locals())
---
+++ django_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\users\rajrp\desktop\git repo\btre_project\btre\django_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
