44df88f3577948de86294e93660795131be9a9c4
Rajrpatel <rajrpatel62499@gmail.com>
2020-04-12 00:01:26 +0530

    static_root changed in settings

--- btre/settings.py
+++ btre/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 """Static files settings for website"""
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "btre/static")]
 
